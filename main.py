469e9996eb8c818282d6a54cf046f7447d48d0a4
Lucas Lourenço Santos <115506713+Lucasrede@users.noreply.github.com>
2024-09-14 01:41:14 +0000

    Pending changes exported from your codespace

---
+++ main.py
@@ -0,0 +1,25 @@
+'''temp = int(input('Entre com a temperatura:'))
+if temp < 0:
+  print ('Congelando...')
+elif 0 <= temp <= 20:
+  print ('Frio')
+elif 21 <= temp <= 25:
+  print ('Normal')
+elif 26 <= temp <= 35:
+  print ('Quente')
+else:
+  print ('Muito quente')'''
+
+#Soma de 0 a 99
+'''s = 0
+for x in range(1, 100):
+  s = s + x
+  print(s)  #só até aqui deu 4950
+
+  if (s > 100):
+    break
+
+  else:
+    continue'''
+
+((7**2)*(4/2))
